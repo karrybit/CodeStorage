1edf407e81436c6498997e6a50cc218653ed8a22
Takumi Karibe <>
2018-06-09 17:44:49 +0900

    no message

--- Learn_You_a_Haskell_for_Great_Good!/heathrow.hs
+++ Learn_You_a_Haskell_for_Great_Good!/heathrow.hs
@@ -10,20 +10,25 @@ heathrowToLondon = [ Section 50 10 30,
 data Label = A | B | C deriving (Show)
 type Path = [(Label, Int)]
 
-optimalPath :: RoadSystem -> Path
-
 roadStep :: (Path, Path) -> Section -> (Path, Path)
 roadStep (pathA, pathB) (Section a b c) =
     let timeA = sum (map snd pathA)
-    let timeB = sum (map snd pathB)
-    forwardTimeToA = timeA + a
-    crossTimeToA = timeB + b + c
-    forwardTimeToB = timeB + b
-    crossTimeToB = timeA + a + c
-    newPathToA = if forwardTimeToA <= crossTimeToA
-                    then (A, a): pathA
-                    else (C, c): (B, b): pathB
-    newPathToB = if forwardTimeToB <= crossTimeToB
-                    then (B, b): pathB
-                    else (C, c): (A, a): pathA
+        timeB = sum (map snd pathB)
+        forwardTimeToA = timeA + a
+        crossTimeToA = timeB + b + c
+        forwardTimeToB = timeB + b
+        crossTimeToB = timeA + a + c
+        newPathToA = if forwardTimeToA <= crossTimeToA
+                        then (A, a): pathA
+                        else (C, c): (B, b): pathB
+        newPathToB = if forwardTimeToB <= crossTimeToB
+                        then (B, b): pathB
+                        else (C, c): (A, a): pathA
     in (newPathToA, newPathToB)
+
+optimalPath :: RoadSystem -> Path
+optimalPath roadSystem =
+    let (bestAPath, bestBPath) = foldl roadStep ([], []) roadSystem
+    in if sum (map snd bestAPath) <= sum (map snd bestBPath)
+        then reverse bestAPath
+        else reverse bestBPath
