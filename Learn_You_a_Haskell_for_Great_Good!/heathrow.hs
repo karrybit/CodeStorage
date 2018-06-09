69f90321b678122e93e481b2dd3f2a8dd31a206c
Takumi Karibe <>
2018-06-09 17:51:20 +0900

    no message

--- Learn_You_a_Haskell_for_Great_Good!/heathrow.hs
+++ Learn_You_a_Haskell_for_Great_Good!/heathrow.hs
@@ -1,3 +1,5 @@
+import Data.List
+
 data Section = Section { getA :: Int, getB :: Int, getC :: Int } deriving (Show)
 type RoadSystem = [Section]
 
@@ -37,3 +39,13 @@ groupsOf :: Int -> [a] -> [[a]]
 groupsOf 0 _ = undefined
 groupsOf _ [] = []
 groupsOf n xs = take n xs: groupsOf n (drop n xs)
+
+main = do
+    contents <- getContents
+    let threes = groupsOf 3 (map read $ lines contents)
+        roadSystem = map (\[a, b, c] -> Section a b c) threes
+        path = optimalPath roadSystem
+        pathString = concat $ map (show . fst) path
+        pathTime = sum $ map snd path
+    putStrLn $ "The best path to take is: " ++ pathString
+    putStrLn $ "Time taken: " ++ show pathTime
