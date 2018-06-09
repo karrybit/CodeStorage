31a7a49208d879055fa44cbf2e6bc48b94ecbd27
Takumi Karibe <>
2018-06-09 17:38:00 +0900

    no message

---
+++ Learn_You_a_Haskell_for_Great_Good!/heathrow.hs
@@ -0,0 +1,29 @@
+data Section = Section { getA :: Int, getB :: Int, getC :: Int } deriving (Show)
+type RoadSystem = [Section]
+
+heathrowToLondon :: RoadSystem
+heathrowToLondon = [ Section 50 10 30,
+                     Section 5 90 20,
+                     Section 40 2 25,
+                     Section 10 8 0]
+
+data Label = A | B | C deriving (Show)
+type Path = [(Label, Int)]
+
+optimalPath :: RoadSystem -> Path
+
+roadStep :: (Path, Path) -> Section -> (Path, Path)
+roadStep (pathA, pathB) (Section a b c) =
+    let timeA = sum (map snd pathA)
+    let timeB = sum (map snd pathB)
+    forwardTimeToA = timeA + a
+    crossTimeToA = timeB + b + c
+    forwardTimeToB = timeB + b
+    crossTimeToB = timeA + a + c
+    newPathToA = if forwardTimeToA <= crossTimeToA
+                    then (A, a): pathA
+                    else (C, c): (B, b): pathB
+    newPathToB = if forwardTimeToB <= crossTimeToB
+                    then (B, b): pathB
+                    else (C, c): (A, a): pathA
+    in (newPathToA, newPathToB)
