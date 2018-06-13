1a676c3669d6ef378997f4beee380d67129309db
Takumi Karibe <takumi.k.5610@gmail.com>
2018-06-13 22:15:59 +0900

    no message

--- Learn_You_a_Haskell_for_Great_Good!/calcBmis.hs
+++ Learn_You_a_Haskell_for_Great_Good!/calcBmis.hs
@@ -1,3 +1,6 @@
 calcBmis :: [(Double, Double)] -> [Double]
 calcBmis xs = [bmi w h | (w, h) <- xs]
     where bmi weight height = weight / height ^ 2
+
+calcBmis' :: [(Double, Double)] -> [Double]
+calcBmis' xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2]
