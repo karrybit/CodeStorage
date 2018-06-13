a07b5176ff9f85a51f36d11597c8c6c167840d86
Takumi Karibe <takumi.k.5610@gmail.com>
2018-06-13 22:26:31 +0900

    no message

--- Learn_You_a_Haskell_for_Great_Good!/describeList.hs
+++ Learn_You_a_Haskell_for_Great_Good!/describeList.hs
@@ -1,5 +1,11 @@
 describeList :: [a] -> String
 describeList ls = "The list is "
-                  ++ case ls of [] -> "empty"
+                  ++ case ls of [] -> "empty."
                                 [_] -> "a singleton list."
                                 xs -> "a longer list."
+
+describeList' :: [a] -> String
+describeList' ls = "The list is " ++ what ls
+    where what []  = "empty."
+          what [_] = "a singleton list."
+          what ls  = "a longer list."
