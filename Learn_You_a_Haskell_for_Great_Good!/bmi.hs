f8132fc8e62ee83546a7681a4a8e2ae5acc2ebdf
Takumi Karibe <takumi.k.5610@gmail.com>
2018-06-13 21:43:32 +0900

    no message

---
+++ Learn_You_a_Haskell_for_Great_Good!/bmi.hs
@@ -0,0 +1,10 @@
+bmiTell :: Double -> Double -> String
+bmiTell weight height
+    | bmi <= skinny = "You're underweight, you emo, you!"
+    | bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly!"
+    | bmi <= fat    = "You're fat! Lose some weight, fatty!"
+    | otherwise     = "You're a whale, congratulations!"
+    where bmi = weight / height ^ 2
+          skinny = 18.5
+          normal = 25.0
+          fat = 30.0
