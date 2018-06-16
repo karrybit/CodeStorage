4b7b036c6fccc800f4e08177f723c823672c35c1
Takumi Karibe <takumi.k.5610@gmail.com>
2018-06-16 17:16:49 +0900

    no message

--- Learn_You_a_Haskell_for_Great_Good!/chain.hs
+++ Learn_You_a_Haskell_for_Great_Good!/chain.hs
@@ -4,3 +4,7 @@ chain n
     | n == 1 = [1]
     | even n = n : chain (n `div` 2)
     | odd n = n : chain (n * 3 + 1)
+
+numLongChains :: Int
+numLongChains = length (filter isLong (map chain [1..100]))
+    where isLong xs = length xs > 15
