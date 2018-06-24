b4c8ff13f48a5c3014a2f68e2ef700a71a589e9d
Takumi Karibe <takumi.k.5610@gmail.com>
2018-06-24 14:09:46 +0900

    no message

--- Learn_You_a_Haskell_for_Great_Good!/phoneBook.hs
+++ Learn_You_a_Haskell_for_Great_Good!/phoneBook.hs
@@ -15,3 +15,6 @@ findKey' key [] = Nothing
 findKey' key ((k,v): xs)
     | key == k = Just v
     | otherwise = findKey' key xs
+
+findKey'' :: (Eq k) => k -> [(k, v)] -> Maybe v
+findKey'' key xs = foldr (\(k, v) acc -> if key == k then Just v else acc) Nothing xs
