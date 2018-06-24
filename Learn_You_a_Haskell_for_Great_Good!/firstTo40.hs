59c01117f252532bf698254f630bd1cde3ababb2
Takumi Karibe <takumi.k.5610@gmail.com>
2018-06-24 14:00:53 +0900

    no message

--- Learn_You_a_Haskell_for_Great_Good!/firstTo40.hs
+++ Learn_You_a_Haskell_for_Great_Good!/firstTo40.hs
@@ -6,3 +6,6 @@ digitSum = sum . map digitToInt . show
 
 firstTo40 :: Maybe Int
 firstTo40 = find (\x -> digitSum x == 40) [1..]
+
+firstTo :: Int -> Maybe Int
+firstTo n = find (\x -> digitSum x == n) [1..]
