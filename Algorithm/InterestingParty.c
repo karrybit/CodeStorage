8594d4b37854e29590b8c2ac3b825a6ff4487624
Takumi Karibe <takumi.k.5610@gmail.com>
2018-06-24 23:26:05 +0900

    no message

--- Algorithm/InterestingParty.c
+++ Algorithm/InterestingParty.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+#define MAX 50
+#define LEN 15
+
+int n;
+char firsts[MAX][LEN];
+char seconds[MAX][LEN];
+
 int main() {
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) scanf("%s", firsts[i]);
+    for (int i = 0; i < n; i++) scanf("%s", seconds[i]);
     return 0;
 }
