6eb4a3f69f44fb219e3d14f2bc431728e8953f02
Takumi Karibe <takumi.k.5610@gmail.com>
2018-06-24 16:08:05 +0900

    no message

--- Learn_You_a_Haskell_for_Great_Good!/shapes.hs
+++ Learn_You_a_Haskell_for_Great_Good!/shapes.hs
@@ -1,8 +1,6 @@
-data Shape = Circle Float Float Float |
-             Rectangle Float Float Float Float
-    deriving (Show)
-
+data Point = Point Float Float deriving (Show)
+data Shape = Circle Point Float | Rectangle Point Point deriving (Show)
 
 area :: Shape -> Float
-area (Circle _ _ r) = pi * r ^ 2
-area (Rectangle x1 y1 x2 y2) = (abs $ x2 - x1) * (abs $ y2 - y1)
+area (Circle _ r) = pi * r ^ 2
+area (Rectangle (Point x1 y1) (Point x2 y2)) = (abs $ x2 - x1) * (abs $ y2 - y1)
