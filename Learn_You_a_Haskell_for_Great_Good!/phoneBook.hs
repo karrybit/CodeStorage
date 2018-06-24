4b910fdd4380ac78f1ae55f0d66af6c0372d10bd
Takumi Karibe <takumi.k.5610@gmail.com>
2018-06-24 14:06:25 +0900

    no message

---
+++ Learn_You_a_Haskell_for_Great_Good!/phoneBook.hs
@@ -0,0 +1,11 @@
+phoneBook =
+    [("betty", "555-2938")
+    ,("bonnie", "452-2928")
+    ,("patsy", "493-2928")
+    ,("lucille", "205-2928")
+    ,("wendy", "939-8282")
+    ,("penny", "853-2492")
+    ]
+
+findKey :: (Eq k) => k -> [(k, v)] -> v
+findKey key xs = snd . head . finlter (\(k, v) -> key == k) $ xs
