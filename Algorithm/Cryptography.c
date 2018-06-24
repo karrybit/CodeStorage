eeaa26904c0287a9707c71c10ca4046de24cee14
Takumi Karibe <takumi.k.5610@gmail.com>
2018-06-25 00:30:30 +0900

    no message

--- Algorithm/Cryptography.c
+++ Algorithm/Cryptography.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) > (b) ? (b) : (a))
+
 int main() {
+    // standard input
+    int n; scanf("%d", &n);
+    int numbers[n];
+    for (int i = 0; i < n; i++) scanf("%d", &numbers[i]);
+
+    // find minimum number in array
+    int minIndex = 0;
+    for (int i = 0; i < n; i++) if (numbers[minIndex] > numbers[i]) minIndex = i;
+    numbers[minIndex]++;
+
+    // calculate
+    unsigned long long int producted = 1;
+    for (int i = 0; i < n; i++) producted *= numbers[i];
+
+    // standard output
+    printf("%llu\n", producted);
+
     return 0;
 }
