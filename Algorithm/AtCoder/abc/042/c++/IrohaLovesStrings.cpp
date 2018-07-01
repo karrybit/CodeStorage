17da7d7eac081a3d9fa60d47db2ce00406f28e8f
Takumi Karibe <takumi.k.5610@gmail.com>
2018-07-01 18:34:06 +0900

    no message

--- Algorithm/AtCoder/abc/042/c++/IrohaLovesStrings.cpp
+++ Algorithm/AtCoder/abc/042/c++/IrohaLovesStrings.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+using std::sort;
 
 int main() {
     int n, l;
-    std::cin >> n >> l;
+    cin >> n >> l;
+
+    vector<string> vs;
+    for (int i = 0; i < n; i++) {
+        string word;
+        cin >> word;
+        vs.push_back(word);
+    }
+
+    sort(vs.begin(), vs.end());
+    for (auto word: vs) cout << word;
+    cout << endl;
+
     return 0;
 }
