29e0587333a557322f2a3c1bba391b17aa4fc7e1
Takumi Karibe <takumi.k.5610@gmail.com>
2018-07-05 03:38:06 +0900

    no message

--- Haskell/Learn_You_a_Haskell_for_Great_Good!/todoList.hs
+++ Haskell/Learn_You_a_Haskell_for_Great_Good!/todoList.hs
@@ -1,5 +1,24 @@
 import System.IO
+import System.Directory
+import Data.List
 
 main = do
-    todoItem <- getLine
-    appendFile "todo.txt" (todoItem ++ "\n")
+    contents <- readFile "todo.txt"
+
+    let todoTasks = lines contents
+        numberedTasks = zipWith (\n line -> show n ++ " - " ++ line) [0..] todoTasks
+
+    putStrLn "These are your TO-DO items:"
+    mapM_ putStrLn numberedTasks
+
+    putStrLn "Which one do you want to delete?"
+    numberString <- getLine
+
+    let number = read numberString
+        newTodoItems = unlines $ delete (todoTasks !! number) todoTasks
+
+    (tempName, tempHandle) <- openTempFile "." "temp"
+    hPutStr tempHandle newTodoItems
+    hClose tempHandle
+    removeFile "todo.txt"
+    renameFile tempName "todo.txt"
