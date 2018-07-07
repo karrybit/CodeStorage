7acb3c72473d38514a2c78c3dbd02b44539f9b3f
Takumi Karibe <takumi.k.5610@gmail.com>
2018-07-07 13:41:06 +0900

    no message

--- Haskell/Learn_You_a_Haskell_for_Great_Good!/todoList.hs
+++ Haskell/Learn_You_a_Haskell_for_Great_Good!/todoList.hs
@@ -1,30 +1,43 @@
+import System.Environment
 import System.IO
 import System.Directory
 import Data.List
 import Control.Exception
 
-main = do
-    contents <- readFile "todo.txt"
+dispatch :: String -> [String] -> IO()
+dispatch "add" = add
+dispatch "view" = view
+dispatch "remove" = remove
 
+main = do
+    (command: argList) <- getArgs
+    dispatch command argList
+
+add :: [String] -> IO()
+add [fileName, todoItem] = appendFile fileName (todoItem ++ "\n")
+
+view :: [String] -> IO()
+view [fileName] = do
+    contents <- readFile fileName
     let todoTasks = lines contents
         numberedTasks = zipWith (\n line -> show n ++ " - " ++ line) [0..] todoTasks
+    putStr $ unlines numberedTasks
 
+remove :: [String] -> IO()
+remove [fileName, numberString] = do
+    contents <- readFile fileName
+    let todoTasks = lines contents
+        numberedTasks = zipWith (\n line -> show n ++ " - " ++ line) [0..] todoTasks
     putStrLn "These are your TO-DO items:"
     mapM_ putStrLn numberedTasks
-
-    putStrLn "Which one do you want to delete?"
-    numberString <- getLine
-
     let number = read numberString
         newTodoItems = unlines $ delete (todoTasks !! number) todoTasks
-
     bracketOnError (openTempFile "." "temp")
         (\(tempName, tempHandle) -> do
             hClose tempHandle
             removeFile tempName)
-
         (\(tempName, tempHandle) -> do
             hPutStr tempHandle newTodoItems
             hClose tempHandle
-            removeFile "todo.txt"
-            renameFile tempName "todo.txt")
+            removeFile fileName
+            renameFile tempName fileName)
