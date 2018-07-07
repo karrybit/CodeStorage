1d2df04fbc278dcc32fb14e70047fad1987e4193
Takumi Karibe <takumi.k.5610@gmail.com>
2018-07-07 13:59:25 +0900

    no message

---
+++ C++/C++_Primer_5th_Edition/GradeCluster.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::vector;
+
+int main() {
+    vector<unsigned> scores(11, 0);
+    unsigned grade;
+    while (cin >> grade) {
+        if (grade <= 100) ++scores[grade/10];
+    }    
+
+    for (auto e: scores) cout << e << " ";
+    cout << endl;
+    return 0;
+}
