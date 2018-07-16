19776e5dd569c2a1664432326246ddac3df28295
Takumi Karibe <takumi.k.5610@gmail.com>
2018-07-16 23:09:38 +0900

    no message

--- Algorithm/Cheetah/chapter5/test/FriendScore/test.sh
+++ Algorithm/Cheetah/chapter5/test/FriendScore/test.sh
@@ -1,30 +1,34 @@
 #!/bin/zsh
 
-read NUMBER
+
 
 CASE_PATH=$HOME/Develop/CodeStorage/Algorithm/Cheetah/chapter5/test/FriendScore
 EXEC_PATH=$HOME/Develop/CodeStorage/Algorithm/Cheetah/chapter5/FriendScore.o
-CASE_FILE="example$NUMBER.txt"
-LINES=0
 
-case $NUMBER in
-[0-4])
+if [ `echo $1 | grep '[0-4]'` ]; then
+    CASE_FILE="example$1.txt"
+    LINES=0
     LINES=`cat $CASE_PATH/$CASE_FILE | wc -l`
-    LINES=$[$LINES - 1];;
-*);;
-esac
+    LINES=$[$LINES - 1]
+    EXPECT=`grep "Return" $CASE_PATH/$CASE_FILE | awk '{print $2}'`
 
+elif [ $1 = "all" ]; then
+    echo "test all cases."
+else
+    echo "👹 The test case number is between 0 and 4. your input is $1"
+    exit 0;
+fi
 
-EXPECT=`grep "Return" $CASE_PATH/$CASE_FILE | awk '{print $2}'`
-
-case $NUMBER in
-[0-4])
+if [ `echo $1 | grep '[0-4]'` ]; then
     ANSWER=`sed -n "1, ${LINES}p" $CASE_PATH/$CASE_FILE | $EXEC_PATH`
-    RESULT="case: $NUMBER, expect: $EXPECT, answer: $ANSWER"
+    RESULT="🚀 case: $1, expect: $EXPECT, answer: $ANSWER"
     if [ ${EXPECT} = ${ANSWER} ];
-        then echo "$RESULT, result: Success"
-        else echo "$RESULT, result: Failure"
+        then echo "$RESULT, result: ☀️ Success"
+        else echo "$RESULT, result: ⛈ Failure"
     fi
-;;
-*) echo "The test case number is between 0 and 4. your input is $NUMBER";;
-esac
+else
+    for i in 0 1 2 3 4
+    do
+        sh $CASE_PATH/test.sh $i
+    done
+fi
