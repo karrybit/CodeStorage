885f269b6533486701706f8cbc86beea9af47284
Takumi Karibe <takumi.k.5610@gmail.com>
2018-07-21 22:51:23 +0900

    no message

--- Algorithm/AtCoder/abc/103/c++/StringRotation.cpp
+++ Algorithm/AtCoder/abc/103/c++/StringRotation.cpp
@@ -10,5 +10,20 @@ int main() {
 
     int length = str1.length();
 
+    bool ok = false;
+    for (int i = 0; i < length + 1; i++) {
+        if (str1 == str2) {
+            ok = true;
+            break;
+        } else {
+            char s = *--str1.end();
+            for (int j = length - 2; j >= 0; j--) str1[j + 1] = str1[j];
+            str1[0] = s;
+        }
+    }
+
+    if (ok) cout << "Yes" << endl;
+    else    cout << "No" << endl;
+
     return 0;
 }
