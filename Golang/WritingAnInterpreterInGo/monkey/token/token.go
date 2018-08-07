5310e0a5bc13159afc25f2348384b76e40067317
Takumi Karibe <takumi.k.5610@gmail.com>
2018-08-08 00:04:04 +0900

    no message

--- Golang/WritingAnInterpreterInGo/monkey/token/token.go
+++ Golang/WritingAnInterpreterInGo/monkey/token/token.go
@@ -16,8 +16,15 @@ const (
 	INT   = "INT"   // 1343456
 
 	// 演算子
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
 
 	// デリミタ
 	COMMA     = ","
