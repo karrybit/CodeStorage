e91ed7db55830d5c762241f3ac1dae77568671cb
Takumi Karibe <takumi.k.5610@gmail.com>
2018-08-12 16:06:18 +0900

    no message

---
+++ Algorithm/Ant/1852/c++/Ants.cpp
@@ -0,0 +1,30 @@
+#import <iostream>
+#import <vector>
+#import <algorithm>
+#import <climits>
+
+using namespace std;
+
+int main() {
+    int L, n;
+    cin >> L >> n;
+
+    vector<int> v;
+    for (int i = 0; i < n; ++i) {
+        int x; cin >> x;
+        v.push_back(x);
+    }
+
+    int minT, maxT;
+    minT = maxT = 0;
+
+    for (int i = 0; i < n; ++i) {
+        minT = max(minT, min(v[i], L - v[i]));
+        maxT = max(maxT, max(v[i], L - v[i]));
+    }
+
+    cout << "min = " << minT << endl;
+    cout << "max = " << maxT << endl;
+
+    return 0;
+}
