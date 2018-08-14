7f3b66f73570e65ac7f554975076bfe47c560403
Takumi Karibe <takumi.k.5610@gmail.com>
2018-08-14 23:52:17 +0900

    add parser

---
+++ Golang/WritingAnInterpreterInGo/monkey/parser/parser.go
@@ -0,0 +1,35 @@
+package parser
+
+import (
+	"../ast"
+	"../lexer"
+	"../token"
+)
+
+// Parser 実体
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+// New Parserの初期化
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	// 2つトークンを読みこむ。curTokenとpeekTokenの両方がセットされる。
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
