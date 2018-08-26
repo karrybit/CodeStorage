14788ba3c143b6f814b48640ce884286d3181b37
Takumi Karibe <takumi.k.5610@gmail.com>
2018-08-26 15:05:05 +0900

    no message

--- Algorithm/Cheetah/chapter7/basic/sample.cpp
+++ Algorithm/Cheetah/chapter7/basic/sample.cpp
@@ -5,10 +5,11 @@ using namespace std;
 
 int h, w, mode;
 unsigned long long int ans = 0;
-int dp[1000007][1000007] = {};
+unsigned long long int dp[1000007][1000007] = {};
 
 unsigned long long int dfs1(int, int);
 unsigned long long int dfs2(int, int);
+void dpCalc();
 
 int main() {
     cin >> h >> w >> mode;
@@ -24,7 +25,7 @@ int main() {
         cout << "ans: " << ans << endl;
         cout << "time: " << msec << " ms" << endl;
 
-    } else {
+    } else if (mode == 1) {
         auto start = chrono::system_clock::now();
         ans = dfs2(0, 0);
         auto end = chrono::system_clock::now();
@@ -33,6 +34,17 @@ int main() {
 
         cout << "memo" << endl;
         cout << "ans: " << ans << endl;
+        cout << "time: " << msec << " ms" << endl;
+
+    } else {
+        auto start = chrono::system_clock::now();
+        dpCalc();
+        auto end = chrono::system_clock::now();
+
+        auto msec = chrono::duration_cast<chrono::milliseconds>(end - start).count();
+
+        cout << "dp" << endl;
+        cout << "ans: " << dp[h][w] << endl;
         cout << "time: " << msec << " ms" << endl;
     }
 
@@ -50,4 +62,15 @@ unsigned long long int dfs2(int nowh, int noww) {
     if (nowh == h && noww == w) return 1;
     if (dp[nowh][noww] != 0) return dp[nowh][noww];
     return dp[nowh][noww] = dfs2(nowh + 1, noww) + dfs2(nowh, noww + 1);
+}
+
+void dpCalc() {
+    int i, j;
+    dp[0][0] = 1;
+    for (i = 0; i <= h; ++i) {
+        for (j = 0; j <= w; ++j) {
+            if (i != 0) dp[i][j] += dp[i - 1][j];
+            if (j != 0) dp[i][j] += dp[i][j - 1];
+        }
+    }
 }
