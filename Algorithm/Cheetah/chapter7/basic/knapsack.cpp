fe9ee60db525513a83f69f44cb77d2babb40beec
Takumi Karibe <takumi.k.5610@gmail.com>
2018-08-26 16:34:25 +0900

    no message

---
+++ Algorithm/Cheetah/chapter7/basic/knapsack.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int weights[5] = { 3, 4, 1, 2, 3 };
+int prices[5] = { 2, 3, 2, 3, 6 };
+int maxw = 10;
+
+int table[6][11];
+int ans = 0;
+
+void nonAlgorithm(int, int, int);
+int recursiceNonMemo(int, int);
+int memo(int, int);
+
+int main() {
+    int mode; cin >> mode;
+    cout << "" << endl;
+
+    for (int i = 0; i < 6; ++i) {
+        for (int j = 0; j < 11; ++j) {
+            table[i][j] = -1;
+        }
+    }
+
+    if (mode == 0) {
+        auto start = chrono::system_clock::now();
+        nonAlgorithm(0, 0, 0);
+        auto end = chrono::system_clock::now();
+
+        auto msec = chrono::duration_cast<chrono::milliseconds>(end - start).count();
+
+        cout << "[not algorithm]" << endl;
+        cout << "ans: " << ans << endl;
+        cout << "time: " << msec << " ms" << endl;
+
+    } else if (mode == 1) {
+        auto start = chrono::system_clock::now();
+        ans = recursiceNonMemo(0, 0);
+        auto end = chrono::system_clock::now();
+
+        auto msec = chrono::duration_cast<chrono::milliseconds>(end - start).count();
+
+        cout << "[recursive not memo]" << endl;
+        cout << "ans: " << ans << endl;
+        cout << "time: " << msec << " ms" << endl;
+
+    } else if (mode == 2) {
+        auto start = chrono::system_clock::now();
+        ans = memo(0, 0);
+        auto end = chrono::system_clock::now();
+
+        auto msec = chrono::duration_cast<chrono::milliseconds>(end - start).count();
+        
+        cout << "[memo]" << endl;
+        cout << "ans: " << ans << endl;
+        cout << "time: " << msec << " ms" << endl;
+    }
+
+    return 0;
+}
+
+void nonAlgorithm(int n, int w, int p) {
+    if (w > maxw) return;
+    ans = max(ans, p);
+    if (n >= 5) return;
+    nonAlgorithm(n + 1, w, p);
+    nonAlgorithm(n + 1, w + weights[n], p + prices[n]);
+}
+
+int recursiceNonMemo(int n, int w) {
+    if (w > maxw) return -100;
+    if (n >= 5) return 0;
+    return max(recursiceNonMemo(n + 1, w), recursiceNonMemo(n + 1, w + weights[n]) + prices[n]);
+}
+
+int memo(int n, int w) {
+    if (w > maxw) return -100;
+    if (n >= 5) return 0;
+    if (table[n][w] >= 0) return table[n][w];
+    return table[n][w] = max(memo(n + 1, w), memo(n + 1, w + weights[n]) + prices[n]);
+}
