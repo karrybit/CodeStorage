394ad8ed82cda3d4bced279beab18ae9068f1a79
Takumi Karibe <takumi.k.5610@gmail.com>
2018-08-29 03:14:32 +0900

    no message

--- Algorithm/AtCoder/abc/084/c++/2017-LikeNumber.cpp
+++ Algorithm/AtCoder/abc/084/c++/2017-LikeNumber.cpp
@@ -5,41 +5,28 @@
 using namespace std;
 
 // 素数判定済み配列
-bool primeTable[MAX] = {false};
+bool prime[MAX];
 // 累積和
-int cumulativeTable[MAX] = {0};
-// クエリ
-int query[MAX][2];
+int sum[MAX];
 
 void calcPrime();
 bool isPrime(int);
 
 int main() {
+    calcPrime();
     int q; cin >> q;
 
     for (int i = 0; i < q; ++i) {
         int l, r; cin >> l >> r;
-        query[i][0] = l;
-        query[i][1] = r;
-    }
-
-    calcPrime();
-
-    for (int i = 0; i < q; ++i) {
-        cout << (cumulativeTable[query[i][1]] - cumulativeTable[query[i][0] - 1]) << endl;
-    }
-
-    cout << "" << endl;
-    for (int i = 0; i < 100; ++i) {
-        cout << cumulativeTable[i] << endl;
+        cout << (sum[r] - sum[l - 1]) << endl;
     }
 
     return 0;
 }
 
 void calcPrime() {
-    primeTable[2] = true;
-    cumulativeTable[2] = 1;
+    prime[2] = true;
+    sum[2] = 1;
 
     for (int p = 3; p < MAX; ++p) {
 
@@ -55,21 +42,19 @@ void calcPrime() {
         }
 
         // 素数にtrueを立てるため、反転させる
-        primeTable[p] = !isComposite;
+        prime[p] = !isComposite;
 
-        if (primeTable[p] || p == 2017) {
+        if (prime[p]) {
             // 素数 || 2017
-            cumulativeTable[p] = cumulativeTable[p - 1] + isPrime(p);
+            sum[p] = sum[p - 1] + (isPrime(p) ? 1 : 0);
 
         } else {
             // 合成数 && (not 2017)
-            cumulativeTable[p] = cumulativeTable[p - 1];
+            sum[p] = sum[p - 1];
         }
-
-        cumulativeTable[p + 1] = cumulativeTable[p];
     }
 }
 
 bool isPrime(int i) {
-    return (primeTable[i] && primeTable[(i + 1) / 2]) || i == 2017;
+    return prime[i] && prime[(i + 1) / 2];
 }
