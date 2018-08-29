ce933c4758e2618d0ed3d0cb8180f68605f11f55
makanai <takumi.k.5610@gmail.com>
2018-08-29 14:02:17 +0900

    no message

--- Algorithm/Cheetah/chapter7/CorporationSalary.cpp
+++ Algorithm/Cheetah/chapter7/CorporationSalary.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
+vector<string> vc;
+int salaries[64];
+
+
 int main() {
+    int n; cin >> n;
+    string str;
+
+    for (int i = 0; i < n; ++i) {
+        cin >> str;
+        vc.push_back(str);
+    }
+
+    // 給料を初期化
+    for (int i = 0; i < n; ++i) salaries[i] = 0;
+
     return 0;
 }
