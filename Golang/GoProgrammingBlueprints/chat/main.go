adf8d4e48f0b379a0d9bf88a25f23d3a0cc267b0
Takumi Karibe <takumi.k.5610@gmail.com>
2018-08-31 02:55:45 +0900

    no message

---
+++ Golang/GoProgrammingBlueprints/chat/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
+)
+
+func main() {
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+
+	// Webサーバーを開始します
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServeHTTPはHTTPリクエストを処理します
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, nil)
+}
