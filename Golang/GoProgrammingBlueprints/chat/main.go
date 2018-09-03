9f071080cca77f2206b9914e2a8dd83b54de40f2
Takumi Karibe <takumi.k.5610@gmail.com>
2018-09-03 18:04:20 +0900

    no message

--- Golang/GoProgrammingBlueprints/chat/main.go
+++ Golang/GoProgrammingBlueprints/chat/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 	"path/filepath"
@@ -8,7 +9,11 @@ import (
 	"text/template"
 )
 
+var port = ":8080"
+
 func main() {
+	var addr = flag.String("addr", port, "アプリケーションのアドレス")
+	flag.Parse() // フラグを解釈します
 	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
@@ -17,7 +22,8 @@ func main() {
 	go r.run()
 
 	// Webサーバーを開始します
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	log.Println("Webサーバーを開始します。ポート：", *addr)
+	if err := http.ListenAndServe(port, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 }
@@ -33,5 +39,5 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
