ce13b71594381aac048dd20ce7c6e22c7d0faac6
Takumi Karibe <takumi.k.5610@gmail.com>
2018-09-03 16:21:25 +0900

    no message

--- Algorithm/Cheetah/chapter7/CorporationSalary.cpp
+++ Algorithm/Cheetah/chapter7/CorporationSalary.cpp
@@ -6,10 +6,12 @@ using namespace std;
 
 vector<string> vc;
 int salaries[64];
+int n;
 
+int memo(int);
 
 int main() {
-    int n; cin >> n;
+    cin >> n;
     string str;
 
     for (int i = 0; i < n; ++i) {
@@ -20,5 +22,18 @@ int main() {
     // 給料を初期化
     for (int i = 0; i < n; ++i) salaries[i] = 0;
 
+    int ans = 0;
+    for (int i = 0; i < n; ++i) ans += memo(i);
+
+    cout << ans << endl;
+
     return 0;
+}
+
+int memo(int index) {
+    // すでに計算済みであれば、それを返却する
+    if (salaries[index] != 0) return salaries[index];    
+    int salary = 0;
+    for (int i = 0; i < n; ++i) if (vc[index][i] == 'Y') salary += memo(i);
+    return salaries[index] = (salary == 0 ? 1 : salary);
 }
