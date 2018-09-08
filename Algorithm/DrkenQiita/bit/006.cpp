f6191652812335f13d4643ebcaf61c55b996f287
Takumi Karibe <takumi.k.5610@gmail.com>
2018-09-08 15:59:25 +0900

    no message

--- Algorithm/DrkenQiita/bit/006.cpp
+++ Algorithm/DrkenQiita/bit/006.cpp
@@ -4,30 +4,26 @@
 using namespace std;
 
 int main() {
-    int n = 5;
+    int n = 5; // 1<<n : 0b100000
 
     // {0, 1, ..., n-1} の部分集合の全探索
-    for (int bit = 0; bit < (1<<n); ++bit)
-    {
-        /* bit で表される集合の処理を書く */
+    for (int bit = 0; bit < (1<<n); ++bit) {
 
-
-        /* きちんとできていることを確認してみる */
         // bit の表す集合を求める
         vector<int> S;
         for (int i = 0; i < n; ++i) {
-            if (bit & (1<<i)) { // i が bit に入るかどうか
-                S.push_back(i);
-            }
+            // i番目のbitが立っていれば、iをpush_backする
+            if (bit & (1<<i)) S.push_back(i);
         }
 
-        // bit の表す集合の出力
         if (bit < 10) cout << bit << ":  { ";
         else          cout << bit << ": { ";
         
+        // bit の表す集合の出力
         for (int i = 0; i < (int)S.size(); ++i) {
             cout << S[i] << " ";
         }
+
         cout << "}" << endl;
     }
 }
