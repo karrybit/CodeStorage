c1e969a0012d707482f9b0f319a01f149ebedd37
makanai <takumi.k.5610@gmail.com>
2018-09-18 20:36:00 +0900

    no message

--- Golang/WritingAnInterpreterInGo/monkey/repl/repl.go
+++ Golang/WritingAnInterpreterInGo/monkey/repl/repl.go
@@ -27,7 +27,7 @@ func Start(in io.Reader, out io.Writer) {
 
 		program := p.ParseProgram()
 		if len(p.Errors()) != 0 {
-			printParseErrors(out, p.Errors())
+			printParserErrors(out, p.Errors())
 			continue
 		}
 
@@ -36,7 +36,23 @@ func Start(in io.Reader, out io.Writer) {
 	}
 }
 
-func printParseErrors(out io.Writer, errors []string) {
+const MONKEY_FACE = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, MONKEY_FACE)
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
 	for _, msg := range errors {
 		io.WriteString(out, "\t"+msg+"\n")
 	}
