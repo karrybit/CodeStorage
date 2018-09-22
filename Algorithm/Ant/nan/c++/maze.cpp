52ec6d1e8a297cf99363d211f6e4cfdfa4cb0ad8
Takumi Karibe <takumi.k.5610@gmail.com>
2018-09-22 13:23:24 +0900

    bfs写経

--- Algorithm/Ant/nan/c++/maze.cpp
+++ Algorithm/Ant/nan/c++/maze.cpp
@@ -1,42 +1,61 @@
 #include <iostream>
-#include <vector>
-#include <string>
+#include <utility>
+#include <queue>
+#include <climits>
 
 using namespace std;
 
+typedef pair<int, int> P;
+
+char maze[10][11];
 int n, m;
-vector<string> maze;
+int sx, sy;
+int gx, gy;
 
-int vy[] = {1, 0, -1, 0};
-int vx[] = {0, 1, 0, -1};
+int d[10][10];
+int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
 
-int bfs(int i, int j, int turn) {
-    maze[i][j] = '#';
-    ++turn;
+int bfs() {
+    queue<P> que;
+    // すべての点をINT_MAXで初期化
+    for (int i = 0; i < n; ++i) for (int j = 0; j < m; ++j) d[i][j] = INT_MAX;
 
-    /// Goalの判定
-    for (int k = 0; k < 4; ++k) {
-        int ny = i - vy[k], nx = j + vx[k];
-        if (ny >= 0 && ny < n && nx >= 0 && nx < m && maze[ny][nx] == 'G') return turn;
-    }    
-    
-    /// 探索
-    for (int k = 0; k < 4; ++k) {
-        int ny = i - vy[k], nx = j + vx[k];
-        if (ny >= 0 && ny < n && nx >= 0 && nx < m && maze[ny][nx] == '.') return bfs(ny, nx, turn);
+    // スタート地点をキューに入れ、その点の距離を0にする
+    que.push(P(sx, sy));
+    d[sx][sy] = 0;
+
+    // キューが空になるまでループ
+    while (que.size()) {
+        // キューの先頭を取り出す
+        P p = que.front(); que.pop();
+        // 取り出してきた状態がゴールなら探索をやめる
+        if (p.first == gx && p.second == gy) break;
+
+        // 移動4方向をループ
+        for (int i = 0; i < 4; ++i) {
+            // 移動した後の点を（nx, ny）とする
+            int nx = p.first + dx[i], ny = p.second + dy[i];
+
+            // 移動が可能かの判定とすでに訪れたことがあるかの判定（d[nx][ny] != MAX_INTなら訪れたことがある）
+            if (0 <= nx && nx < n && 0 <= ny && ny < m && maze[nx][ny] != '#' && d[nx][ny] == INT_MAX) {
+                // 移動できるならキューに入れ、その点の距離をpからの距離+1で確定する
+                que.push(P(nx, ny));
+                d[nx][ny] = d[p.first][p.second] + 1;
+            }
+        }
     }
-
-    return 0;
+    return d[gx][gy];
 }
 
 int main() {
     cin >> n >> m;
-    maze.resize(n);
-    for (int i = 0; i < n; ++i) cin >> maze[i];
-
     for (int i = 0; i < n; ++i) for (int j = 0; j < m; ++j) {
-        if (maze[i][j] == 'S') cout << bfs(i, j, 0) << endl;
+        cin >> maze[i][j];
+        if (maze[i][j] == 'S') sx = i, sy = j;
+        if (maze[i][j] == 'G') gx = i, gy = j;
     }
+
+    cout << bfs() << endl;
 
     return 0;
 }
