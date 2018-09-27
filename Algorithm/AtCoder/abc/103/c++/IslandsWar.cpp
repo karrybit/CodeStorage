fcf7963b87ea7efef334102c27591707d84e0982
makanai <takumi.k.5610@gmail.com>
2018-09-27 14:48:54 +0900

    wa

--- Algorithm/AtCoder/abc/103/c++/IslandsWar.cpp
+++ Algorithm/AtCoder/abc/103/c++/IslandsWar.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <utility>
+#include <algorithm>
+#include <climits>
 
 using namespace std;
 
@@ -9,10 +11,24 @@ typedef pair<int, int> P;
 int main() {
     int n, m; cin >> n >> m;
     vector<P> bridges;
-    bridges.resize(m);    
+    bridges.resize(m);
     for (int i = 0; i < m; ++i) {
         int l, r; cin >> l >> r;
         bridges[i] = P(l, r);
     }
+
+    sort(bridges.begin(), bridges.end());
+
+    int ans = 1;
+    int s = bridges[0].first;
+    for (int i = 0; i < m; ++i) {
+        if (i + 1 == bridges[i].second && s >= bridges[i].first) {
+            ++ans;
+            s = bridges[i].second;
+        }
+    }
+
+    cout << ans << endl;
+
     return 0;
 }
