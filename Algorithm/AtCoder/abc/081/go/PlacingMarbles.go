c14e0d9abe728d78375037e2b22b173ca2f5e54e
Takumi Karibe <takumi.k.5610@gmail.com>
2018-10-06 18:14:33 +0900

    no message

---
+++ Algorithm/AtCoder/abc/081/go/PlacingMarbles.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var s string
+	fmt.Scan(&s)
+
+	ans := 0
+	for i := 0; i < len(s); i++ {
+		if s[i] == '1' {
+			ans++
+		}
+	}
+
+	fmt.Println(ans)
+}
