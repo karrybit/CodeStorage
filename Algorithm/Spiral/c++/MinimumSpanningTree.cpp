78ec20f4d7bd88eb96e9edd3ed222820bbe48c47
Takumi Karibe <takumi.k.5610@gmail.com>
2018-10-24 17:08:18 +0900

    segmentation fault

--- Algorithm/Spiral/c++/MinimumSpanningTree.cpp
+++ Algorithm/Spiral/c++/MinimumSpanningTree.cpp
@@ -59,13 +59,65 @@ void printvp(vector<pair<T, T> > vp) {
 vvi table;
 int n;
 
+#define WHITE 0
+#define GRAY 1
+#define BLACK 2
+
+int prim() {
+    int u, minv;
+    vi d(n), p(n), color(n);
+
+    for (int i = 0; i < n; ++i) {
+        d[i] = INT_MAX;
+        p[i] = INT_MAX;
+        color[i] = WHITE;
+    }
+
+    d[0] = 0;
+
+    while (true) {
+        minv = INT_MAX;
+        u = -1;
+        for (int i = 0; i < n; ++i) {
+            if (minv > d[i] && color[i] != BLACK) {
+                u = i;
+                minv = d[i];
+            }
+        }
+
+        if (u == -1) break;
+        color[u] = BLACK;
+        for (int v = 0; v < n; ++v) {
+            if (color[v] != BLACK && table[u][v] != INT_MAX) {
+                if (d[v] > table[u][v]) {
+                    d[v] = table[u][v];
+                    p[v] = u;
+                    color[v] = GRAY;
+                }
+            }
+        }
+    }
+
+    int sum = 0;
+    for (int i = 0; i < n; ++i) {
+        if (p[i] != -1) sum += table[i][p[i]];
+    }
+
+    return sum;
+}
+
 int main() {
     cin >> n;
     table.resize(n);
     for (int i = 0; i < n; ++i) {
-        int cost; cin >> cost;
-        cost = cost < 0 ? INT_MAX : cost;
-        table[i].push_back(cost);
+        for (int j = 0; j < n; ++j) {
+            int cost; cin >> cost;
+            cost = cost < 0 ? INT_MAX : cost;
+            table[i].push_back(cost);
+        }
     }
+
+    cout << prim() << endl;
+
     return 0;
 }
