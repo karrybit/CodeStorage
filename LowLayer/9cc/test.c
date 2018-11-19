e59d410ae3f70494991ea73b9f78725cd631b2bf
Takumi Karibe <takumi.k.5610@gmail.com>
2018-11-20 01:14:11 +0900

    no message

--- LowLayer/9cc/test.c
+++ LowLayer/9cc/test.c
@@ -1,7 +1,17 @@
-int plus(int x, int y) {
-	return x + y;
-}
+#include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-	return plus(3, 4);
+int main(int argc, char **argv) {
+	if (argc != 2) {
+		fprintf(stderr, "引数の個数が正しくありません\n");
+		return 1;
+	}
+
+	printf(".intel_syntax noprefix\n");
+	printf(".global main\n");
+	printf("main:\n");
+	printf("	mov rax, %d\n", atoi(argv[1]));
+	printf("	ret\n");
+
+	return 0;
 }
