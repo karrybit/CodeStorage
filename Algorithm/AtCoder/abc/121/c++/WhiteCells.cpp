d5c2bd6123a3ca88117a3b23fab7b60483b4b62d
Takumi Karibe <takumi.k.5610@gmail.com>
2019-03-09 22:05:53 +0900

    no message

---
+++ Algorithm/AtCoder/abc/121/c++/WhiteCells.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <utility>
+#include <climits>
+#include <bitset>
+#include <cmath>
+
+#define MOD 1000000007
+
+using namespace std;
+
+typedef long long ll;
+
+typedef vector<ll> vl;
+typedef vector<int> vi;
+typedef vector<vl> vvl;
+typedef vector<vi> vvi;
+
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pi;
+
+typedef vector<pll> vpl;
+typedef vector<pi> vpi;
+
+typedef queue<ll> ql;
+typedef queue<int> qi;
+
+template <class T>
+void printv(vector<T> vc) {
+    for (auto e: vc) cout << e << " ";
+    cout << endl;
+}
+
+template <class T>
+void printvv(vector<T> vvc) { 
+    for (auto vc: vvc) {
+        for (auto e: vc) cout << e << " ";
+        cout << endl;
+    }
+}
+
+template <class T>
+void printvp(vector<pair<T, T> > vp) {
+    for (auto pT: vp) cout << pT.first << " " << pT.second << endl;
+}
+
+int main() {
+    int H, W, h, w;
+    cin >> H >> W;
+    cin >> h >> w;
+    cout << (H - h) * (W - w) << endl;
+    return 0;
+}
